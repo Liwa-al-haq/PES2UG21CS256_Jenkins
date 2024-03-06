19d6d79e29f3b72a73b7a509af027686b12efaff
Liwa al haq <97494103+Liwa-al-haq@users.noreply.github.com>
2024-03-06 09:51:29 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(void) {
   cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
+  cout << "Hello, Silver\n";
   cout << "I have successfully built and run\n";
   return 0;
 }
