72bc6d8ab12aa493acda5f7329049b7927e0e4de
Liwa al haq <97494103+Liwa-al-haq@users.noreply.github.com>
2024-03-06 10:21:59 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Pipeline\n";
+  cout << "Hello, Silver\n";
+  cout << "I have successfully built and run\n";
+  return 0;
+}
